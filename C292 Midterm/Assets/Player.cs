c43596dec0c18cac679d1a917883486e1afac50a
ajbognar <abognar@iu.edu>
2021-12-07 23:25:23 -0500

    Final Milestone

--- C292 Midterm/Assets/Player.cs
+++ C292 Midterm/Assets/Player.cs
@@ -7,10 +7,15 @@ public class Player : MonoBehaviour
 {
 
     [SerializeField] float _lrSpeed = 6;
+    [SerializeField] float _ratSpeed = 10;
     [SerializeField] float _jumpSpeed = 5;
+    [SerializeField] float _ratJumpSpeed = 8;
     [SerializeField] Transform GroundPos;
+    [SerializeField] Transform RatPos;
+    [SerializeField] AudioSource jumpSound;
     private Rigidbody2D rb;
     private bool isGrounded = false;
+    private bool isRatted = false;
     private float jumpTimer = 0f;
     // Start is called before the first frame update
 
@@ -26,6 +31,9 @@ public class Player : MonoBehaviour
             isGrounded = true;
             jumpTimer = 0;
         }
+        if (Physics2D.OverlapCircleAll(RatPos.position, .05f, LayerMask.GetMask("Rat")).Length > 0) {
+            isRatted = true;
+        }
         Move();
         Jump();
         Death();
@@ -33,7 +41,12 @@ public class Player : MonoBehaviour
 
     void Move() {
         float dir = Input.GetAxis("Horizontal");
-        rb.velocity = new Vector3(dir * _lrSpeed, rb.velocity.y, 0);
+        if (isRatted) {
+            rb.velocity = new Vector3(dir * _ratSpeed, rb.velocity.y, 0);
+        }
+        else {
+            rb.velocity = new Vector3(dir * _lrSpeed, rb.velocity.y, 0);
+        }
 
         if (dir < 0) {
             transform.localScale = new Vector3(-1, 1, 1);
@@ -44,10 +57,19 @@ public class Player : MonoBehaviour
     }
     
     void Jump() {
-        if (Input.GetButtonDown("Jump") && isGrounded) {
-            GetComponent<Rigidbody2D>().velocity = new Vector3(0, _jumpSpeed, 0);
+        if (Input.GetButtonDown("Jump") && isRatted) {
+            GetComponent<Rigidbody2D>().velocity = new Vector3(0, _ratJumpSpeed, 0);
+            jumpSound.Play();
             isGrounded = false;
+            isRatted = false;
         }
+        else if (Input.GetButtonDown("Jump") && isGrounded) {
+            GetComponent<Rigidbody2D>().velocity = new Vector3(0, _jumpSpeed, 0);
+            jumpSound.Play();
+            isGrounded = false;
+            isRatted = false;
+        }
+
         if (isGrounded == false) {
             jumpTimer += Time.deltaTime;
         }
@@ -56,6 +78,7 @@ public class Player : MonoBehaviour
     void OnDrawGizmosSelected()
     {
         Gizmos.DrawSphere(GroundPos.position, .05f);
+        Gizmos.DrawSphere(RatPos.position, .025f);
     }
 
     void Death() {
